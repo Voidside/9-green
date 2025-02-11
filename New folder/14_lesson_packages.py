2a1c89cf3e604cb73bf13419f74a52e85399047f
Voidside <marcusspersony@gmail.com>
2025-02-11 09:16:27 +0500

    a

--- New folder/14_lesson_packages.py
+++ New folder/14_lesson_packages.py
@@ -39,20 +39,20 @@ link -> https://www.w3schools.com/python/python_datetime.asp
 # print(f"Sana: {hozir.strftime("A")}")
 
 # Sanadan sanani ayirish
-bugun = datetime.date.today()
-ramazon = datetime.date(2025, 3, 1)
+# bugun = datetime.date.today()
+# ramazon = datetime.date(2025, 3, 1)
 
 # print(ramazon-bugun)
 
 
 # Vaqtdan vaqtni ayirish
-bugun1 =    datetime.datetime.now()
-ramazon = datetime.datetime(2025, 3, 1, 00, 00, 1)
+# bugun1 =    datetime.datetime.now()
+# ramazon = datetime.datetime(2025, 3, 1, 00, 00, 1)
 
 # print(bugun1)
 # print(ramazon)
 
-farq = ramazon-bugun1
+# farq = ramazon-bugun1
 # print(farq)
 # print(farq.days)
 
@@ -68,7 +68,7 @@ farq = ramazon-bugun1
 
 
 
-hozir = datetime.datetime.now()
+# hozir = datetime.datetime.now()
 
 
 
@@ -77,35 +77,50 @@ hozir = datetime.datetime.now()
 # t_kun = input(f"ni kiriting:")
 
 
-t_yil = 2009
-t_oy = 12
-t_kun = 30
+# t_yil = 2009
+# t_oy = 12
+# t_kun = 30
 
-t_sana = datetime.date(t_yil, t_oy, t_kun)
+# t_sana = datetime.date(t_yil, t_oy, t_kun)
 
 # bugun = datetime.
 
 
-bugun1 =    datetime.datetime.now()
+# bugun1 =    datetime.datetime.now()
 
 
-print(f"Bugun sana va vaqt:{bugun1.day} soat{bugun1.hour}:{bugun1.minute}")
+# print(f"Bugun sana va vaqt:{bugun1.day} soat{bugun1.hour}:{bugun1.minute}")
 
-print(f"Bugun sana: {bugun1.day}")
+# print(f"Bugun sana: {bugun1.day}")
 
-print(f"Bugun vaqt: {bugun1.hour}:{bugun1.minute}")
-
-
+# print(f"Bugun vaqt: {bugun1.hour}:{bugun1.minute}")
 
 
-year = int(input("Type in year: "))
-month = int(input("Type in month: "))
-day = int(input("Type in day: "))
 
-date = datetime.datetime(year, month, day)
 
-if date.year < bugun.year or date.month < bugun.month:
-    print("!")
-else:
-    result = date-bugun
-    print(f"")
+# year = int(input("Type in year: "))
+# month = int(input("Type in month: "))
+# day = int(input("Type in day: "))
+
+# date = datetime.datetime(year, month, day)
+
+# if date.year < bugun.year or date.month < bugun.month:
+#     print("404 Not found!")
+# else:
+#     result = date-bugun
+#     print(f"Days left: {result}")
+
+
+
+today = datetime.datetime.now()
+
+
+
+b_year = 2009
+b_month = 12
+b_day = 30
+
+b_date = datetime.date(b_month, b_month, b_day)
+
+if b_date.month < today.month:
+    pass
