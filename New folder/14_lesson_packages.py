50a87ece3cb5fe20831cb2fd33020de037c8fd0d
Voidside <marcusspersony@gmail.com>
2025-02-20 10:56:14 +0500

    Pattern

--- New folder/14_lesson_packages.py
+++ New folder/14_lesson_packages.py
@@ -129,9 +129,86 @@ today = datetime.date.today()
 #     print(f"Happy birthday!")
 
 
-today = datetime.date.today()
-day = int(input("Type in days: "))
+# today = datetime.date.today()
+# day = int(input("Type in days: "))
 
-date = today + datetime.timedelta(days=day)
+# date = today + datetime.timedelta(days=day)
 
-print(f"After {day} days date will be: {date.day}-{date.strftime("%B")} {date.year}-year. Weekday: {date.strftime("%A")}")
+# print(f"After {day} days date will be: {date.day}-{date.strftime("%B")} {date.year}-year. Weekday: {date.strftime("%A")}")
+
+"""
+link -> https://www.w3schools.com/python/module_math.asp
+link -> https://docs.python.org/3/library/math.html
+"""
+
+
+""" RegEx - Regular Expressions """
+import re 
+
+# world1 = "python"
+# world2 = "Hello"
+# world3 = "notebook"
+
+# print(re.match("^p....n", world1))
+# print(re.match("^H...o", world2))
+# print(re.match("^n......k", world3))
+
+
+# ism = input("Ismingizni kiriting: ")
+
+# if re.match("^M", ism):
+#     print(f"Salom {ism} sizning  ismingiz M harfi bn boshlanadi.")
+# else: 
+#     print(f"Salom {ism}")
+
+
+
+# Extract phone number from a string
+# extract_phone_number_pattern = "\\+998[1-9][0-9]{7,14}"
+# re.findall(extract_phone_number_pattern, 'You can reach me out at +12223334444 and +56667778888') # returns ['+12223334444', '+56667778888']
+
+
+
+import re
+
+# phone_number_pattern = "^[+]998[\s][0-9]{2}[\s][0-9]{3}[-][0-9]{2}[-][0-9]{2}"
+# tel = input("Nomer kiriting: ")
+# if re.match(phone_number_pattern, tel):
+#     print("Siz to'g'ri raqam kiritdingiz")
+# else:
+#     print(f"Siz noto'g'ri raqam kiritdingiz")
+
+
+
+"""
+link -> https://docs.python.org/3/howto/regex.html
+link -> https://docs.python.org/3/library/re.html
+link -> https://www.w3schools.com/python/python_regex.asp
+"""
+# matn = """
+#     fake@gmail.com
+# """
+
+
+# email_pattern = "[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+"
+# email = re.search(email_pattern, matn)
+# print(email)
+
+
+
+
+""" 1 """
+word = "python aaaaa  bbbb ddddd"
+word_pattern = r'\b[a-z]{5}\b'
+
+print(re.findall(word_pattern, word))
+
+
+
+""" 2 """
+date_pattern = r"[0-9]{4}[-][0-9]{2}[-][0-9]{2}"
+stt = """2025-02-20 2009-12-30"""
+print(re.findall(date_pattern, stt))
+
+
+""" 3 """
