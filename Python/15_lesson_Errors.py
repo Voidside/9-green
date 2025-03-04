7eaca1ff58ce44bb6685c117dc7f7ea15b98a6c1
Voidside <marcusspersony@gmail.com>
2025-03-04 08:35:43 +0500

    tidying

--- Python/15_lesson_Errors.py
+++ Python/15_lesson_Errors.py
@@ -27,40 +27,43 @@
 
 
 """ Calculator """
-def cal():
-    """ """
-    while True:
-            try:
-                num1 = input("Son kiriting(Chiqish uchun 'exit'): ")
-                if num1 == "exit":
-                    break
-                sign = input("Amal kiriting(Chiqish uchun 'exit'): ")
-                if num1 == "exit":
-                    break
-                num2 = input("Son kiriting(Chiqish uchun 'exit'): ")
-                if num1 == "exit":
-                    break
-            except ValueError:
-                 print(f"Siz faqat son kiritishingiz mumkin!")
-            if num2.isdigit():
-                if sign == "+":
-                    print(f"{int(num1+num2)}")
-                elif sign == "-":
-                    print(f"{int(num1-num2)}")
-                elif sign == "*":
-                    print(f"{int(num1*num2)}")
-                elif sign == ":":
-                    try:
-                        print(f"{int(num1/num2)}")
-                    except ZeroDivisionError:
-                        print(f"Sonni nolga bolish mumkin emas!")
-                else:
-                    print(f"Value error")
+# def cal():
+#     """ """
+#     while True:
+#             try:
+#                 num1 = input("Son kiriting(Chiqish uchun 'exit'): ")
+#                 if num1 == "exit":
+#                     break
+#                 sign = input("Amal kiriting(Chiqish uchun 'exit'): ")
+#                 if num1 == "exit":
+#                     break
+#                 num2 = input("Son kiriting(Chiqish uchun 'exit'): ")
+#                 if num1 == "exit":
+#                     break
+#             except ValueError:
+#                  print(f"Siz faqat son kiritishingiz mumkin!")
+#             if num2.isdigit():
+#                 if sign == "+":
+#                     print(f"{int(num1+num2)}")
+#                 elif sign == "-":
+#                     print(f"{int(num1-num2)}")
+#                 elif sign == "*":
+#                     print(f"{int(num1*num2)}")
+#                 elif sign == ":":
+#                     try:
+#                         print(f"{int(num1/num2)}")
+#                     except ZeroDivisionError:
+#                         print(f"Sonni nolga bolish mumkin emas!")
+#                 else:
+#                     print(f"Value error")
             
             
 
-print(cal())
+# print(cal())
 
 # num1 = int(input("Son kiriting: "))
 # amal = input("Amal kiriting: ")
 # num2 = int(input("Son kiriting: "))
+
+
+print(1/3)
