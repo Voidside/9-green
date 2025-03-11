257dbc2026980ce3d0a1a217f9c8c2877b9500c5
Voidside <marcusspersony@gmail.com>
2025-03-11 09:15:40 +0500

    r

--- Re exams/re_exam4.py
+++ Re exams/re_exam4.py
@@ -3,30 +3,30 @@ import datetime
 """ 1 """
 bugun = datetime.datetime.now()
 
-print(bugun.strftime("%w"))
+# print(bugun.strftime("%w"))
 
 
 """ 2 """
-print(bugun.strftime("%B"))
+# print(bugun.strftime("%B"))
 
 
 """ 3 """
-print(bugun.strftime("%a"))
+# print(bugun.strftime("%a"))
 
 
 """ 4 """
-print(f"Current time & date: {bugun.year}/{bugun.month}-{bugun.strftime("%B")}/{bugun.hour}:{bugun.minute}")
+# print(f"Current time & date: {bugun.year}/{bugun.month}-{bugun.strftime("%B")}/{bugun.hour}:{bugun.minute}")
 
 
 """ 5 """
-year = int(input("Type in year: "))
-month = int(input("Type in month: "))
-day = int(input("Type in day: "))
+# year = int(input("Type in year: "))
+# month = int(input("Type in month: "))
+# day = int(input("Type in day: "))
 
-date = datetime.datetime(year, month, day)
+# date = datetime.datetime(year, month, day)
 
-if date.year < bugun.year or date.month < bugun.month:
-    print("!")
-else:
-    result = date-bugun
-    print(f"")
+# if date.year < bugun.year or date.month < bugun.month:
+#     print("!")
+# else:
+#     result = date-bugun
+#     print(f"")
--- Re exams/revision.py
+++ Re exams/revision.py
@@ -1,61 +1,102 @@
 """ 9G016 """
 """ 1 """
-def my_ranje(start, stop, step):
-    """ Sonli oraliq """
+# def my_ranje(start, stop, step):
+#     """ Sonli oraliq """
      
-    result = [start]
+#     result = [start]
 
-    for son in result:
-        son =+ 1
+#     for son in result:
+#         son =+ 1
 
 
 
 """ 2 """
-class Inson():
+# class Homosapiens():
+#     """ """
+#     def __init__(self, name: str, surname: str, age: int, b_year: int, pho_num: int, address: str, prof: str):
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+#         self.b_year = b_year
+#         self.pho_num = pho_num
+#         self.address = address
+#         self.prof = prof
+
+
+
+#     def name1(self):
+#         """ Name """
+#         return f"{self.name.title()}"
+    
+
+
+#     def surname1(self):
+#         """ Surname """
+#         return f"{self.surname.title()}"
+    
+
+
+#     def age1(self):
+#         """ Age """
+#         return f"{self.age}"
+    
+
+
+#     def pho_num1(self):
+#         """ Phone number """
+#         return f"{self.pho_num}"
+    
+
+
+#     def address1(self):
+#         """ Address """
+#         return f"{self.address}"
+    
+
+
+#     def get_info(self):
+#         """ All info """
+#         return f"Name: {self.name.title()} \nSurname: {self.surname.title()} \nAge: {self.age} \nBirth year: {self.b_year} \nPhone number: {self.pho_num} \nManzil: {self.address}"
+    
+# object1 = Homosapiens("Abdulahad", "Ubaydullayev", 15, 2009, 998771170073, "Namangan", "IT (Junior)")
+
+# print(object1.name1())
+# print(object1.surname1())
+# print(object1.age1())
+# print(object1.pho_num1())
+# print(object1.get_info())
+
+
+
+
+""" Student """
+
+class Student(): 
     """ """
-    def __init__(self, ism: str, familiya: str, yosh: int, t_yil: int, pho_num: int, address: str, prof: str):
-        self.ism = ism
-        self.familiya = familiya
-        self.yosh = yosh
-        self.t_yil = t_yil
-        self.pho_num = pho_num
+    def __init__(self, name: str, surname: str, fath_name: str, school: str, grade: int, address: str, marks: list, classes:list, friends: list):
+        self.name = name
+        self.surname = surname
+        self.fath_name = fath_name
+        self.school = school
+        self.grade = grade
         self.address = address
-        self.prof = prof
+        self.marks = marks
+        self.classes = classes
+        self.friends = friends
 
 
 
-    def ism1(self):
-        """ Ism """
-        return f"{self.ism.title()}"
+    def get_overall_ball(self):
+        """ """
+        return sum(self.marks)
     
 
 
-    def familiya1(self):
-        """ Familiya """
-        return f"{self.familiya.title()}"
+    def get_mid_ball(self):
+        """ """
+        return sum(self.marks)/len(self.marks)
     
 
 
-    def yosh1(self):
-        """ Yosh """
-        return f"{self.yosh}"
-    
 
-
-    def pho_num1(self):
-        """ Phone number """
-        return f"{self.pho_num}"
-    
-
-
-    def address1(self):
-        """ Address """
-        return f"{self.address}"
-    
-
-
-    def get_info(self):
-        """ All info """
-        return f"Ism: {self.ism.title()}\n Familiya: {self.familiya.title()}\n Yosh: {self.yosh}\n Tel. raqam: {self.pho_num}\n Manzil: {self.address}"
-    
-
+object1 = Student("Abdulahad", "Ubaydullayev", "Iskandar", "BM", 9, "Namangan", [32,55,12,54,64,33], ["de", "are", "sde"], ["f","r","i"])
