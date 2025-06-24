1635e8cb633359c77e77761af876c852846d0839
Voidside <marcusspersony@gmail.com>
2025-06-24 14:05:30 +0500

    a

--- Django projects/project/mainapp/serializers.py
+++ Django projects/project/mainapp/serializers.py
@@ -1,14 +1,31 @@
 from .models import *
-from rest_framework import serializers
+# from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
-class Category_Serializer(serializers.HyperlinkedModelSerializer):
+
+
+class Category_Serializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'photo', 'about']
 
 
 
-class Product_Serializer(serializers.HyperlinkedModelSerializer):
+class Product_Serializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['category', 'name', 'made_by', 'weight_mark', 'weight', 'photo',\
+                   'serial_number', 'made_on', 'expiration_date', 'about']
+
+
+# class Category_Serializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Category
+#         fields = '__all__'
+
+
+
+# class Product_Serializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
